0871838a3af68204a83c4f3d447a65e7c897058d
SandersJ <sandersjack141@gmail.com>
2024-05-01 21:50:35 +0200

    fix to actions

--- Scripts/createPlot.py
+++ Scripts/createPlot.py
@@ -10,7 +10,7 @@ df = df.drop_duplicates(subset='Date', keep="last")
 df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
 df.set_index('Date', inplace=True)
 
-ax = df.plot(x="Date", y="WC")
+ax = df.plot(y="WC")
 ax.set_title("Word Count vs Date")
 plt.savefig("analysis/WordcountVsDate.pdf", format="pdf", bbox_inches="tight")
 
