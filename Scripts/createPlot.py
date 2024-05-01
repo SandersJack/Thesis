1b3d43e0a856568209dffe4d05e64aaea3af959a
SandersJ <sandersjack141@gmail.com>
2024-05-01 21:39:49 +0200

    Updates to wc script

--- Scripts/createPlot.py
+++ Scripts/createPlot.py
@@ -7,6 +7,9 @@ df = pd.read_csv('analysis/wordcount.csv')
 plt.rcParams['figure.constrained_layout.use'] = True
 
 df = df.drop_duplicates(subset='Date', keep="last")
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+
 ax = df.plot(x="Date", y="WC")
 ax.set_title("Word Count vs Date")
 plt.savefig("analysis/WordcountVsDate.pdf", format="pdf", bbox_inches="tight")
