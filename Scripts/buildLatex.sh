5932785baccbc530ad94c7f19dc374756f54008d
JackLaptop <sandersjack141@gmail.com>
2024-05-15 12:35:25 +0200

    Added better latex building so all figures and lists are corectly constructed over all machines. Also added nodemon to run during development so the latex is built on save

---
+++ Scripts/buildLatex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pdflatex -interaction=nonstopmode $1.tex
+bibtex $1
+pdflatex -interaction=nonstopmode $1.tex
+pdflatex -interaction=nonstopmode $1.tex
+
+### Move files to build 
+mv $1.pdf build/$1.pdf
+mv $1.log build/$1.log
+mv $1.aux build/$1.aux
+mv $1.bbl build/$1.bbl
+mv $1.out build/$1.out
+mv $1.toc build/$1.toc
+mv $1.lof build/$1.lof
+mv $1.lot build/$1.lot
+mv $1.blg build/$1.blg
+###
