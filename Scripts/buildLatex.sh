fa5bd4b18195defbac26275480563fa18609b2bf
Jack Sanders <sandersjack141@gmail.com>
2025-02-19 18:47:27 +0000

    Added gloss. Need to check that all have been inclulded

--- Scripts/buildLatex.sh
+++ Scripts/buildLatex.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pdflatex -interaction=nonstopmode $1.tex
+makeglossaries $1
 bibtex $1
 pdflatex -interaction=nonstopmode $1.tex
 pdflatex -interaction=nonstopmode $1.tex
@@ -15,4 +16,11 @@ mv $1.toc build/$1.toc
 mv $1.lof build/$1.lof
 mv $1.lot build/$1.lot
 mv $1.blg build/$1.blg
+mv $1.acn build/
+mv $1.acr build/
+mv $1.ist build/
+mv $1.alg build/
+mv $1.glg build/
+mv $1.glo build/
+mv $1.gls build/
 ###
